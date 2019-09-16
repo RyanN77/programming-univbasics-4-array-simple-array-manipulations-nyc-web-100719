eb08c142c783d0fa6c8109d32aeaa0ce830a922e
adventurous-class-0665 <rngao97@gmail.com>
2019-09-16 19:45:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,8 +21,27 @@ end
 def shift_with_args(array)
   array.shift(2)
 end
-
+#test fail here
 def using_concat(array1, array2)
   array1 += array2.concat 
 end
 
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
